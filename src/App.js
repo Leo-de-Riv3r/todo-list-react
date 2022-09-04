9d3bbac7d9ef98d0bb00abaaeed87cbe5d082864
Leo-de-Riv3r <leoguti182@gmail.com>
2022-09-04 20:09:42 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} width="200px" className="App-logo" alt="logo" />
+        <div className="container">
+          <h1 className="title">
+            Hello World
+            <span aria-label="emoji" role="img">
+              🔥
+            </span>
+          </h1>
+          <input type="text" className="new-task" />
+        </div>
       </header>
     </div>
   );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: "",
+    };
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+      newTask: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <input
+        onChange={this.handleTaskChange}
+        type="text"
+        className="new-task"
+      />
+    );
+  }
 }
 
 export default App;
